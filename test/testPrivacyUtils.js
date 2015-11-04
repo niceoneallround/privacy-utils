659ba87beba213fbd3dfdbdf612623d765445000
niceoneallround <niceoneallround@gmail.com>
2015-11-03 16:30:34 -0800

    Only have one value in @type

--- test/testPrivacyUtils.js
+++ test/testPrivacyUtils.js
@@ -4,10 +4,8 @@
 // Test JSONLD utils
 //
 var assert = require('assert'),
-    jsonldUtils = require('jsonld-utils/lib/jldUtils'),
     should = require('should'),
-    PPN_T =  require('data-models/lib/PrivacyPNDataModel').TYPE,
-    PPN_TAG =  require('data-models/lib/PrivacyPNDataModel').TAG,
+    PPN_OCTX = require('data-models/lib/PrivacyPNDataModel').OCTX,
     privacyUtils = require('../lib/privacyUtils'),
     util = require('util');
 
@@ -16,40 +14,34 @@ describe('privacy utils tests', function() {
 
   describe('1 create privacy value tests', function() {
 
-    it('1.1 create with all props - tags an array', function() {
+    it('1.1 create with octx an array', function() {
       var props, pv;
 
       props = {};
       props.evalue = '23';
-      props.octx = 'http://myotype';
-      props.tags = [PPN_TAG.OpaqueToken];
+      props.octx = [PPN_OCTX.AETNA_POC1];
       pv = privacyUtils.createValue(props);
       assert(pv, util.format('no privacy value returned for props:%j', props));
 
       pv.should.have.property('@value', props.evalue);
       pv.should.have.property('@type');
-      assert(jsonldUtils.isType(pv, PPN_T.Obfuscated), util.format('No PPN_TAG.Obfuscated:%j', pv));
-      assert(jsonldUtils.isType(pv, PPN_TAG.OpaqueToken), util.format('No PPN_TAG.OpaqueToken:%j', pv));
-      assert(jsonldUtils.isType(pv, props.octx), util.format('No props.octx:%j', pv));
-
+      assert((pv['@type'].length === 1), util.format('expected type length of 1 got:%s pv is:%j', pv['@type'].length, pv));
+      pv['@type'][0].should.be.equal(PPN_OCTX.AETNA_POC1);
       assert(privacyUtils.isObfuscated(pv), util.format('value is not isObfuscted:%j', pv));
     });
 
-    it('1.2 create with all props - tags an object', function() {
+    it('1.2 create with octx and instance', function() {
       var props, pv;
 
       props = {};
-      props.evalue = '23';
-      props.octx = 'http://myotype';
-      props.tags = PPN_TAG.OpaqueToken;
+      props.evalue = '56';
+      props.octx = PPN_OCTX.AETNA_POC1;
       pv = privacyUtils.createValue(props);
       assert(pv, util.format('no privacy value returned for props:%j', props));
 
       pv.should.have.property('@value', props.evalue);
-      pv.should.have.property('@type');
-      assert(jsonldUtils.isType(pv, PPN_T.Obfuscated), util.format('No PPN_TAG.Obfuscated:%j', pv));
-      assert(jsonldUtils.isType(pv, PPN_TAG.OpaqueToken), util.format('No PPN_TAG.OpaqueToken:%j', pv));
-      assert(jsonldUtils.isType(pv, props.octx), util.format('No props.octx:%j', pv));
+      pv.should.have.property('@type', PPN_OCTX.AETNA_POC1);
+      assert(privacyUtils.isObfuscated(pv), util.format('value is not isObfuscted:%j', pv));
     });
 
   }); // describe 1
